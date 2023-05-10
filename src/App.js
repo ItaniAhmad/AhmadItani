b92b220d87e06f484358d4a6b272f0699caaa2eb
ItaniAhmad <ahmaditani11.2000@gmail.com>
2023-05-10 14:01:59 +0300

    added

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './App.css';
+import Home from './Components/pages/Home';
+import Navbar from'./Components/Navbar';
+import{ BrowserRouter as Router,Routes  ,Route } from 'react-router-dom';
+import ScrollToTop from "./ScrollToTop";
+import About from './Components/pages/About';
+function App() {
+  return (
+    < >
+    <Router>
+      <Navbar />
+      <ScrollToTop />
+      <Routes >
+      <Route path='/' exact element={<Home/>} />
+      <Route path='/About' element={<About/>} />
+      
+      </Routes>
+    </Router>
+    </>
+  );
+}
+
+export default App;
